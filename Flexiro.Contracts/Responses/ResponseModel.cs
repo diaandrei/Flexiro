b3efc210da91e60de77c42c9493ff2efbf42c798
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-06 21:01:58 +0000

    Adding a response model

---
+++ Flexiro.Contracts/Responses/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Flexiro.Contracts.Responses
+{
+    public class ResponseModel<T> where T : class
+    {
+        public ResponseModel()
+        {
+            Success = false;
+        }
+        public bool Success { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ExceptionMessage { get; set; }
+        public T Content { get; set; }
+    }
+}
