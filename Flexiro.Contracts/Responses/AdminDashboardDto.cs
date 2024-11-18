0066784a4f71d6a42a9632dc19b9e263f884bff5
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-18 17:55:35 +0000

    Adding responses for admin dashboard and for shop details

---
+++ Flexiro.Contracts/Responses/AdminDashboardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Flexiro.Contracts.Responses
+{
+    public class AdminDashboardDto
+    {
+        public required IList<ShopDetailsDto> ActiveShops { get; set; }
+        public required IList<ShopDetailsDto> PendingShops { get; set; }
+        public required IList<ShopDetailsDto> InactiveShops { get; set; }
+        public required IList<ShopDetailsDto> AllShops { get; set; }
+    }
+}
---
+++ Flexiro.Contracts/Responses/ShopDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿using Flexiro.Application.Models;
+
+namespace Flexiro.Contracts.Responses
+{
+    public class ShopDetailsDto
+    {
+        public int ShopId { get; set; }
+        public required string ShopLogo { get; set; }
+        public required string ShopName { get; set; }
+        public required string OwnerName { get; set; }
+        public required string ShopDescription { get; set; }
+
+        public ShopAdminStatus AdminStatus { get; set; }
+        public decimal TotalEarnings { get; set; }
+        public int TotalOrders { get; set; }
+    }
+}
