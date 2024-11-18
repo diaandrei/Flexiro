bddc9ba5ce81ef15fefd9911cdee870c5868cb8d
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-18 17:59:23 +0000

    Adding a dto response for the wishlist

---
+++ Flexiro.Contracts/Responses/UserWishlistResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Flexiro.Contracts.Responses
+{
+    public class UserWishlistResponseDto
+    {
+        public int ProductId { get; set; }
+        public int ShopId { get; set; }
+        public required string UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
