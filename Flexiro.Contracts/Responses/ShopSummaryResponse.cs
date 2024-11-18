fa87f074bc99fdf6e6c719d441d0867e867d91b7
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-18 17:57:24 +0000

    Adding a response object for shop summary

---
+++ Flexiro.Contracts/Responses/ShopSummaryResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Flexiro.Contracts.Responses
+{
+    public class ShopSummaryResponse
+    {
+        public int ShopId { get; set; }
+        public required string ShopName { get; set; }
+        public required string ShopLogo { get; set; }
+        public required string ShopDescription { get; set; }
+    }
+}
