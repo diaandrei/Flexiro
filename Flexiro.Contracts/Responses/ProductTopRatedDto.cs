c689aa3e03f70d74962b85027f72dc154e6df177
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-18 18:03:35 +0000

    Adding responses for product top rated and summary

---
+++ Flexiro.Contracts/Responses/ProductTopRatedDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Flexiro.Contracts.Responses
+{
+    public class ProductTopRatedDto
+    {
+        public int ProductId { get; set; }
+        public required string ProductName { get; set; }
+        public required string Description { get; set; }
+        public decimal PricePerItem { get; set; }
+        public required string ShopImage { get; set; }
+    }
+}
