d286b08cfec77991562b42ed621452d0a006b566
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-18 18:12:36 +0000

    Adding a reponse for customer dashboard

---
+++ Flexiro.Contracts/Responses/CustomerDashboardResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Flexiro.Contracts.Responses
+{
+    public class CustomerDashboardResponse
+    {
+        public required IList<ShopSummaryResponse> Shops { get; set; }
+        public required IList<ProductSaleResponseDto> SaleProducts { get; set; }
+        public required List<ProductTopRatedDto> TopRatedAffordableProducts { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+    }
+}
