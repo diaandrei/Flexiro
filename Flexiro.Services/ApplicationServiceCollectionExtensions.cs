b3669a91d42666166eb6c78048b866f741ef59cd
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-19 18:13:17 +0000

    Injecting the product service in add application method

--- Flexiro.Services/ApplicationServiceCollectionExtensions.cs
+++ Flexiro.Services/ApplicationServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Flexiro.Services.Services.Interfaces;
+﻿using Flexiro.Services.Services;
+using Flexiro.Services.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Flexiro.Services
@@ -8,6 +9,7 @@ namespace Flexiro.Services
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddScoped<IShopService, ShopService>();
+            services.AddScoped<IProductService, ProductService>();
 
             return services;
         }
