16685aa2d23a25ef624b11fe6203fbbb0d6bd813
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-19 17:43:54 +0000

    Creating a new interface for the product service

---
+++ Flexiro.Services/Services/Interfaces/IProductService.cs
@@ -0,0 +1,24 @@
+﻿using Flexiro.Application.DTOs;
+using Flexiro.Application.Models;
+using Flexiro.Contracts.Requests;
+using Flexiro.Contracts.Responses;
+
+namespace Flexiro.Services.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ResponseModel<ProductResponseDto>> CreateProductAsync(ProductCreateDto productDto);
+        Task<ResponseModel<ProductResponseDto>> UpdateProductAsync(int productId, ProductUpdateDto productDto);
+        Task<bool> DeleteProductAsync(int productId);
+        Task<ResponseModel<ProductListsDto>> GetAllProductsAsync();
+        Task<ResponseModel<ProductDetailResponseDto>> GetProductDetailsByIdAsync(int productId, string baseUrl);
+        Task<ResponseModel<IEnumerable<ProductResponse>>> GetProductsByShopIdAsync(int shopId, string baseUrl);
+        Task<ResponseModel<IEnumerable<ProductResponseDto>>> GetProductsByCategoryIdAsync(int categoryId);
+        Task<ResponseModel<IEnumerable<ProductResponseDto>>> SearchProductsByNameAsync(string productName);
+        Task<ResponseModel<UserWishlistResponseDto>> AddProductToWishlistAsync(int productId, string userId, int shopId);
+        Task<ResponseModel<string>> ChangeProductStatusAsync(int productId, ProductStatus newStatus);
+        Task UpdateProductImagePaths(int productId, List<string> imagePaths);
+        Task<ResponseModel<List<ProductSaleResponseDto>>> GetSaleProductsAsync();
+        Task<List<ProductTopRatedDto>> GetTopRatedAffordableProductsAsync(string baseUrl);
+    }
+}
