60f511701d7cd0597856892a2afd0d6e4499c9a8
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-19 17:39:54 +0000

    Adding an order dto

---
+++ Flexiro.Application/DTOs/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Flexiro.Application.DTOs
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal PricePerItem { get; set; }
+        public float Weight { get; set; }
+        public int MinimumPurchase { get; set; }
+        public string ProductCondition { get; set; }
+        public int Stock { get; set; }
+        public bool ImportedItem { get; set; }
+        public List<string> Images { get; set; }
+        public string Description { get; set; }
+        public List<string> Videos { get; set; }
+        public List<string> Tags { get; set; }
+        public string SKU { get; set; }
+    }
+}
