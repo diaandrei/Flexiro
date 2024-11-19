630253516d4f83ca621428fd9af267709dd17190
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-19 17:39:01 +0000

    Adding dto's around the order

---
+++ Flexiro.Application/DTOs/OrderDetailDto.cs
@@ -0,0 +1,28 @@
+﻿using Flexiro.Application.Models;
+
+namespace Flexiro.Application.DTOs
+{
+    public class OrderDetailDto
+    {
+        public int OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public string UserId { get; set; }
+        public decimal ItemsTotal { get; set; }
+        public decimal ShippingCost { get; set; }
+        public decimal Tax { get; set; }
+        public decimal TotalAmount { get; set; }
+        public OrderStatus Status { get; set; }
+        public string PaymentStatus { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+
+        // Shipping and Billing Address details
+        public ShippingAddressDto ShippingAddress { get; set; }
+        public BillingAddressDto BillingAddress { get; set; }
+
+        // List of order items associated with the order
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+
+}
---
+++ Flexiro.Application/DTOs/OrderItemDto.cs
@@ -0,0 +1,18 @@
+﻿using Flexiro.Application.Models;
+
+namespace Flexiro.Application.DTOs
+{
+    public class OrderItemDto
+    {
+        public int OrderDetailsId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal PricePerUnit { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+    }
+}
