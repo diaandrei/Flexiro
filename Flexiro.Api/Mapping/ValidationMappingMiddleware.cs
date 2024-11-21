c159d4afc4f3c133fc626a87a29b4ea4570f5185
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-11-21 17:18:32 +0000

    Adding the validation mapping middleware

---
+++ Flexiro.Api/Mapping/ValidationMappingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Flexiro.Contracts.Responses;
+
+namespace Flexiro.API.Middleware
+{
+    public class ValidationMappingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ValidationMappingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (FluentValidation.ValidationException ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                var validationFailureResponse = new ValidationFailureResponse
+                {
+                    Errors = ex.Errors.Select(x => new ValidationResponse
+                    {
+                        PropertyName = x.PropertyName,
+                        Message = x.ErrorMessage
+                    })
+                };
+
+                await context.Response.WriteAsJsonAsync(validationFailureResponse);
+            }
+        }
+    }
+}
