cfe1eebdfc1b3f2815bc2e6e89837a0b62d93ba8
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-12-02 15:18:14 +0000

    Adding the service implementation of the shipping

---
+++ Flexiro.Services/Services/Interfaces/IShippingService.cs
@@ -0,0 +1,14 @@
+﻿using Flexiro.Application.Models;
+using Flexiro.Contracts.Requests;
+using Flexiro.Contracts.Responses;
+
+namespace Flexiro.Services.Services.Interfaces
+{
+    public interface IShippingService
+    {
+        Task<ResponseModel<ShippingAddress>> GetShippingAddressByUserIdAsync(string userId);
+        Task<ResponseModel<ShippingAddress>> AddShippingAddressAsync(AddUpdateShippingAddressRequest request);
+        Task<ResponseModel<ShippingAddress>> UpdateShippingAddressAsync(int addressId, AddUpdateShippingAddressRequest request);
+        Task<ResponseModel<IList<ShippingAddress>>> GetAddressBookByUserIdAsync(string userId);
+    }
+}
