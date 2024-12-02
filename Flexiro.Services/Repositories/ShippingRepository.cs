534e704569e23cb295ab9319d840bbd63246a3b1
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-12-02 15:14:53 +0000

    Adding the interface and the implementation for adding, updating and fetching user address

---
+++ Flexiro.Services/Repositories/ShippingRepository.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using EasyRepository.EFCore.Generic;
+using Flexiro.Application.Models;
+using Flexiro.Contracts.Requests;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flexiro.Services.Repositories
+{
+    public class ShippingRepository : IShippingRepository
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ShippingRepository(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<ShippingAddress> GetShippingAddressByUserIdAsync(string userId)
+        {
+            return (await _unitOfWork.Repository
+                .GetQueryable<ShippingAddress>(a => a.UserId == userId)
+                .FirstOrDefaultAsync())!;
+        }
+
+        public async Task<ShippingAddress> AddShippingAddressAsync(AddUpdateShippingAddressRequest request)
+        {
+            var newAddress = _mapper.Map<ShippingAddress>(request);
+
+            await _unitOfWork.Repository.AddAsync(newAddress);
+            await _unitOfWork.Repository.CompleteAsync();
+
+            return newAddress;
+        }
+
+        public async Task<ShippingAddress> UpdateShippingAddressAsync(int addressId, AddUpdateShippingAddressRequest request)
+        {
+            var existingAddress = await _unitOfWork.Repository
+                .GetQueryable<ShippingAddress>(a => a.ShippingAddressId == addressId)
+                .FirstOrDefaultAsync();
+
+            if (existingAddress == null)
+            {
+                return null!; // Returning null if the address does not exist
+            }
+
+            // Map the request data into the existing address
+            _mapper.Map(request, existingAddress);
+
+            // Update the address in the database
+            _unitOfWork.Repository.Update(existingAddress);
+            await _unitOfWork.Repository.CompleteAsync();
+
+            return existingAddress;
+        }
+
+        public async Task<IList<ShippingAddress>> GetAddressBookByUserIdAsync(string userId)
+        {
+            return await _unitOfWork.Repository
+                .GetQueryable<ShippingAddress>(a => a.UserId == userId)
+                .ToListAsync();
+        }
+    }
+}
