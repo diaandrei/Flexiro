cc642d9e94b9c36efdd12b6f7e8141a02ea4b078
Andrei Diamandescu <diamandescuandrei@gmail.com>
2024-12-05 19:13:23 +0000

    Updating the controller and the order service to use the new methods and the blob storage

---
+++ Flexiro.Contracts/Responses/GroupedOrdersDto.cs
@@ -0,0 +1,17 @@
+﻿using Flexiro.Application.DTOs;
+
+namespace Flexiro.Contracts.Responses
+{
+    public class GroupedOrdersDto
+    {
+        public List<OrderDetailDto> NewOrders { get; set; } = new();
+        public List<OrderDetailDto> PendingOrders { get; set; } = new();
+        public List<OrderDetailDto> ProcessingOrders { get; set; } = new();
+        public List<OrderDetailDto> ShippedOrders { get; set; } = new();
+        public List<OrderDetailDto> DeliveredOrders { get; set; } = new();
+        public List<OrderDetailDto> CanceledOrders { get; set; } = new();
+        public List<OrderDetailDto> ReturnedOrders { get; set; } = new();
+        public List<OrderDetailDto> CompletedOrders { get; set; } = new();
+        public List<OrderDetailDto> AllOrders { get; set; } = new();
+    }
+}
--- Flexiro.Services/Services/Interfaces/IOrderService.cs
+++ Flexiro.Services/Services/Interfaces/IOrderService.cs
@@ -7,12 +7,13 @@ namespace Flexiro.Services.Services.Interfaces
 {
     public interface IOrderService
     {
-        Task<ResponseModel<OrderResponseDto>> PlaceOrderAsync(string userId, AddUpdateShippingAddressRequest shippingAddressDto);
+        Task<ResponseModel<OrderResponseDto>> PlaceOrderAsync(string userId, AddUpdateShippingAddressRequest shippingAddressDto, string paymentMethod);
         Task<int> GetTotalOrdersByShopAsync(int shopId);
         Task<(List<Order>, int)> GetDeliveredOrdersByShopAsync(int shopId);
         Task<(List<string>, int)> GetAllCustomersByShopAsync(int shopId);
         Task<decimal> GetTotalEarningsByShopAsync(int shopId);
         Task<int> GetNewOrderCountByShopAsync(int shopId);
-        Task<List<OrderDetailDto>> GetOrdersByShopAsync(int shopId);
+        Task<GroupedOrdersDto> GetGroupedOrdersByShopAsync(int shopId);
+        Task<bool> UpdateOrderStatusAsync(UpdateOrderStatusDto request);
     }
 }
