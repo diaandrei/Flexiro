3a23e50b8bb0433a654a2481a7bccfd94ddd7fc9
Andrei Diamandescu <diamandescuandrei@gmail.com>
2025-01-02 20:34:56 +0000

    Adding a repository to handle the app notifications

---
+++ Flexiro.Services/Repositories/INotificationRepository.cs
@@ -0,0 +1,11 @@
+﻿using Flexiro.Application.Models;
+
+namespace Flexiro.Services.Repositories
+{
+    public interface INotificationRepository
+    {
+        Task AddNotificationAsync(Notification notification);
+        Task<List<Notification>> GetUserNotificationsAsync(string userId);
+        Task MarkNotificationsAsReadAsync(string userId);
+    }
+}
---
+++ Flexiro.Services/Repositories/NotificationRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using EasyRepository.EFCore.Generic;
+using Flexiro.Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flexiro.Services.Repositories
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public NotificationRepository(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            await _unitOfWork.Repository.AddAsync(notification);
+            await _unitOfWork.Repository.CompleteAsync();
+        }
+
+        public async Task<List<Notification>> GetUserNotificationsAsync(string userId)
+        {
+            return await _unitOfWork.Repository
+                .GetQueryable<Notification>()
+                .Where(n => n.UserId == userId)
+                .OrderByDescending(n => n.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task MarkNotificationsAsReadAsync(string userId)
+        {
+            var notifications = await _unitOfWork.Repository
+                .GetQueryable<Notification>()
+                .Where(n => n.UserId == userId && !n.IsRead)
+                .ToListAsync();
+
+            foreach (var notification in notifications)
+            {
+                notification.IsRead = true;
+            }
+
+            await _unitOfWork.Repository.CompleteAsync();
+        }
+    }
+}
